c466f14b696829e7725ff0f4f9a1d98138bfd7ea
shepard.wang <shepard.wang@ebaotech.com>
2023-01-03 13:43:55 +0800

    func

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ow_swear_helper
+for no good use 😈
+
+create a dict.json file beside the exe file and write json for your replacement.\neg: {"a":"o","b":"k"}
+run this software in cmd and it will replace your clipboard according to your your dict.json.
+you can type something and copy it, wait 1 second and paste back.
---
+++ main.py
@@ -0,0 +1,25 @@
+import pyperclip3
+import json
+from time import sleep
+
+pyperclip3.copy("")
+sleep(1)
+print(
+    'create a dict.json file beside the exe file and write json for your replacement.\neg: {"a":"o","b":"k"}')
+
+replaceDict = {"A":"А","a":"а","B":"В","b":"В","E":"Е","e":"е","K":"К","k":"К","M":"М","m":"М","H":"Н","O":"О","o":"о","P":"Р","p":"р","C":"С","c":"с","T":"Т","y":"у","X":"Х","x":"х"}
+
+try:
+    f = open("./dict.json", "r", encoding='utf-8')
+    wholeText = f.read()
+    f.close()
+    replaceDict = json.loads(wholeText)
+except:
+    print("parse dict.json failed, use default.")
+
+while 1 < 2:
+    content = pyperclip3.paste(text=True)
+    for key in replaceDict:
+        content = content.replace(key, replaceDict[key])
+    pyperclip3.copy(content)
+    sleep(1)
